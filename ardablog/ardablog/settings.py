62d70e385f6c78ad50a433e15ca12ff1ed1ddfe9
ardaellidokuz <82497401+ardaellidokuz@users.noreply.github.com>
2021-07-02 01:22:27 +0300

    Update settings.py

--- ardablog/ardablog/settings.py
+++ ardablog/ardablog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b-62wvs@n2#mp2i)-=51kx7@4&d+9-m_%15-!2$gwkfzhwa)sq'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
